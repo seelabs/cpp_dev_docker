d985156f112f4cea0cf5e99f2f694099537749cc
seelabs <scott.determan@yahoo.com>
2019-01-04 16:58:58 -0500

    Build cmake so it uses the system curl:
    Without this cmake could not use https. Alternately, openssl could have been
    added, but I would like this container to minimize the number of deps it
    installs.

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -4,11 +4,17 @@ set -ex
 
 version=${1}
 
+apt-get update
+apt-get install -y --no-install-recommends \
+        zlib1g-dev \
+        libcurl4-openssl-dev \
+    ;
+
 curl -Lf https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz -o cmake.tar.gz
 tar xf cmake.tar.gz
 rm cmake.tar.gz
 pushd cmake-${version}
-./bootstrap
+./bootstrap --system-curl
 make -j4
 make install
 popd # cmake-${version}
